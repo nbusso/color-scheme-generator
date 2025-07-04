9917902818a08cb33c3bffef4f52b2aa8bcfd3e8
Nicolás Busso <nicolasbusso@gmail.com>
2025-07-04 16:46:14 -0300

    first-commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const colorPicker = document.getElementById('color-picker')
+const modeSelection = document.getElementById('mode-selection')
+const generatorForm = document.getElementById('generator-form')
+const colorShowcase = document.getElementById('color-showcase')
+let colorsArray = []
+
+let hex = colorPicker.value.slice(1)
+let mode = modeSelection.value
+
+
+colorPicker.addEventListener('change', function(){
+    hex = colorPicker.value.slice(1)
+})
+
+modeSelection.addEventListener('change', function(){
+    mode = modeSelection.value
+})
+
+generatorForm.addEventListener('submit', function(e) {
+    e.preventDefault()
+    getColor()
+})
+
+function renderColors() {
+
+    const html = colorsArray.map(color => {
+        return `
+        <div class="color-container">
+            <div class="color-canvas" style="background-color: ${color.hex.value}" ></div>
+            <div class="color-hex"><p>${color.hex.value}</p></div>
+        </div>
+        `
+    }).join('')
+
+    colorShowcase.innerHTML = html
+}
+
+function getColor() {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}&count=5`, { method: "GET" })
+    .then(res => res.json())
+    .then(data => {
+        colorsArray = data.colors
+        renderColors()
+    })
+}
+
+getColor()
